0240ae69f8492c5dadb61e665cfa90c0ce42c45a
Alexus <77337386+Otakubuns@users.noreply.github.com>
2023-12-08 23:30:36 -0500

    initial commit

---
+++ src/test/java/com/example/AlexusG100727793/AlexusG100727793ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlexusG100727793;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexusG100727793ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
